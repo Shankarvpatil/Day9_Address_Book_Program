cd4d89d7237688415fdbeac27cdd2970f6c9efed
Shankar Vilas Patil <69981881+Shankarvpatil@users.noreply.github.com>
2021-06-30 10:46:20 +0530

    Update Address_Book.java

--- Address_Book.java
+++ Address_Book.java
@@ -1,85 +1,115 @@
 package addressbook;
 
-public class Contact {
-public static void main(String[] args) {
-		
-	}
-	String firstName, lastName, address, city, state, email;
-    long phoneNumber, zipCode;
-    
-    public Contact() {}
+import java.util.Scanner;
 
-    public Contact(String firstName, String lastName, String address, String city, String state, String email, long phoneNumber, long zipCode) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.address = address;
-        this.city = city;
-        this.state = state;
-        this.email = email;
-        this.phoneNumber = phoneNumber;
-        this.zipCode = zipCode;
-    }
-	public String getFirstName() {
-        return firstName;
-    }
+public class AddressBook 		
+		{
+				   private  String FirstName;
+				   private String LastName;
+				   private String State;
+				   private String City;
+				   private String Address;
+				   private long ZipCode;
+				   private long PhoneNO;
+				   private String Email;
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-    public String getLastName() {
-        return lastName;
-    }
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
+				  Scanner userinput = new Scanner(System.in);
+				   public String getFirstName()
+				   {
+				     return FirstName;
+				   }
+				   public void setFirstName(String firstName)
+				   {
+				     FirstName = firstName;
+				   }
+				  public String getLastName()
+				  {
+				     return LastName;
+				  }
+				  public void setLastName(String lastName)
+				  {
+				     LastName = lastName;
+				   }
+				  public String getState() 
+				  {
+				     return State;
+				   }
+				  public void setState(String state)
+				   {
+				     State = state;
+				    }
+				  public String getCity()
+				   {
+				     return City;
+				    }
+				  public void setCity(String city)
+				   {
+				       City = city;
+				    }
+				  public String getAddress()
+				    {
+				       return Address;
+				     }
+				  public void setAddress(String address)
+				   {
+					Address = address;
+				    }
+				  public long getZipCode()
+				   {
+				      return ZipCode;
+				    }
+				  public void setZipCode(long zipCode)
+				    { 
+				        ZipCode = zipCode;
+					}
+				   public long getPhoneNO()
+				    {
+				        return PhoneNO;
+					}
+				   public void setPhoneNO(long phoneNO)
+				   {
+				       PhoneNO = phoneNO;
+				      }
+				   public String getEmail()
+				    {
+					return Email;
+				     }
+				  public void setEmail(String email)
+				   {  
+				       Email = email;
+				      }
+				public void contactDisplay()
+				{
+				    System.out.println("Enter a first name: ");  
+				    setFirstName(userinput.nextLine()); 
+				    System.out.println("Enter a last name: ");
+				    setLastName(userinput.nextLine());
+				    System.out.println("Enter a state: ");
+				    setState(userinput.next());
+				    System.out.println("Enter a city: ");
+				    setCity(userinput.next());
+				    System.out.println("Enter a address: ");
+				    setAddress(userinput.next());
+				    System.out.println("Enter a zipcode: ");
+				    setZipCode(userinput.nextLong());
+				    System.out.println("Enter a phoneno: ");
+				    setPhoneNO(userinput.nextLong());
+				    System.out.println("Enter a email: ");
+				    setEmail(userinput.next());
+				    System.out.println("name: " + this.getFirstName());
+				    System.out.println("last name: " + this.getLastName()); 
+				    System.out.println("state: " + this.getState());
+				    System.out.println("zipcode: " + this.getZipCode());
+				    System.out.println("city: " + this.getCity()); 
+				    System.out.println("phone_no: " + this.getPhoneNO());
+				    System.out.println("email: " + this.getEmail());
+				}
 
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-    	this.city = city;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public long getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(long phoneNumber) {
-    	this.phoneNumber = phoneNumber;
-    }
-
-    public long getZipCode() {
-        return zipCode;
-    }
-
-    public void setZipCode(long zipCode) {
-        this.zipCode = zipCode;
-    }
-
-    }
+				public static void main(String[] args)
+				{
+					System.out.println("Welcome to Address Book Program");
+				   AddressBook user1 = new  AddressBook();
+				   user1.contactDisplay();
+				}
+		}			
